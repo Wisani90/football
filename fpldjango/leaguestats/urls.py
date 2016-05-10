2049eabfb97f27920fc8b542211bf6aee5adcd0d
Ben Keen <Benjamin.Keen@hitachiconsulting.com>
2016-05-10 08:44:08 +0100

    Sort bottom 10 with lowest pts at top, remove eval

--- fpldjango/leaguestats/urls.py
+++ fpldjango/leaguestats/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<league_id>\d+)/$', views.results, name='results'),
+    url(r'^demo/$', views.demo, name='demo'),
     url(r'^find_league_number/$', views.find_league_number, name='find_league_number')
 ]
--- fpldjango/leaguestats/views.py
+++ fpldjango/leaguestats/views.py
@@ -1,6 +1,8 @@
 import requests
+import os
 import json
 
+import ruamel.yaml as yaml
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
@@ -22,9 +24,11 @@ def get(url):
     else:
         return res
 
+__here__ = os.path.dirname(os.path.abspath(__file__))
 
 def index(request):
     league_num_url = request.build_absolute_uri(reverse('find_league_number'))
+    demo_url = request.build_absolute_uri(reverse('demo'))
     if request.method == 'GET':
         form = PostForm()        
     else:
@@ -37,14 +41,33 @@ def index(request):
             return HttpResponseRedirect(reverse('results',
                                                 kwargs={'league_id': league_id}))
 
-    return render(request, 'leaguestats/index.html', {
+    context = {
         'form': form,
         'league_num_url': league_num_url,
-    })
+        'demo_url': demo_url
+    }
+    return render(request, 'leaguestats/index.html', context)
+
+def byteify(_json):
+    """
+    Converts unicode string to byte string
+    for use by c3 javascript library
+    """
+    if isinstance(_json, dict):
+        return {byteify(key): byteify(value)
+                for key, value in _json.iteritems()}
+    elif isinstance(_json, list):
+        return [byteify(element) for element in _json]
+    elif isinstance(_json, unicode):
+        return _json.encode('utf-8')
+    else:
+        return _json
 
 def results(request, league_id):
     res = get(request.build_absolute_uri(reverse('leaguestatsview', kwargs={'league_id': league_id})))
-    data = eval(res.content)
+    data = res.json()
+    data = byteify(data)
+
     context = {
         "cumulative_transfers_made": data['cumulative_transfers_made'],
         "gamepoints_by_week": data['gamepoints_by_week'],
@@ -60,3 +83,25 @@ def results(request, league_id):
 
 def find_league_number(request):
     return render(request, 'leaguestats/find_league_number.html')
+
+def demo(request):
+    filepath = os.path.join(__here__, 'static', 'json', 'demo.json')
+    with open(filepath) as data_json:
+        data = yaml.safe_load(data_json)
+
+    context = {
+        "cumulative_transfers_made": data['cumulative_transfers_made'],
+        "gamepoints_by_week": data['gamepoints_by_week'],
+        "gamepoint_rank": data['gamepoint_rank'],
+        "gamepoint_top_10": data['gamepoint_top_10'],
+        "gamepoint_bottom_10": data['gamepoint_bottom_10'],
+        "overall_point_rank": data['overall_point_rank'],
+        "bench_points_top_10": data['bench_points_top_10'],
+        "normalised_bench_points_top_10": data['normalised_bench_points_top_10'],
+        "team_value_by_week": data['team_value_by_week'],
+    }
+
+    return render(request, 'leaguestats/results.html', context)
+
+
+
