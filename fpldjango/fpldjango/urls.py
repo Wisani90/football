0e916916a46dd28cc4d664ec4023311651006edd
Ben Keen <Benjamin.Keen@hitachiconsulting.com>
2016-05-23 21:14:56 +0100

    add static files to url patterns

--- fpldjango/fpldjango/urls.py
+++ fpldjango/fpldjango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^leaguestats/', include('leaguestats.urls')),
@@ -23,3 +24,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(url='/leaguestats/')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
